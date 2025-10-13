b9e06562b5c33cdaec84656fdd3f4ab359b5dcc6
Keks <keks@htmlacademy.ru>
2025-10-13 11:20:45 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/functions.js
@@ -0,0 +1,27 @@
+const isStringLengthValid  = (string, maxLength) => string.length <= maxLength;
+
+const isStringPalindrome = (string) => {
+  const newString = string.replaceAll(' ', '').toUpperCase();
+  let reverseString = '';
+  for (let i = newString.length - 1; i >= 0; i--) {
+    reverseString += newString[i];
+  }
+  return newString === reverseString;
+};
+
+const extractNumber = (input) => {
+  let result = '';
+
+  input = String(input);
+
+  for (let i = 0; i < input.length; i++) {
+    if (!isNaN(parseInt(input[i], 10))) {
+      result += input[i];
+    }
+  }
+
+  return Number(result);
+};
+
+export {isStringLengthValid, isStringPalindrome, extractNumber};
+
